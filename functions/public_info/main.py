6f8f909e88f5611bfbb7dfbe7d578b3a5438179e
Richard Grover <38992201+richgrov@users.noreply.github.com>
2025-07-08 19:23:23 -0600

    Fetch from database

--- functions/public_info/main.py
+++ functions/public_info/main.py
@@ -1,5 +1,13 @@
+from datetime import datetime, timedelta
+import json
+
 import functions_framework
+from google.cloud import firestore
+
+client = firestore.Client(database="status-db")
 
 @functions_framework.http
 def main(request):
-    return "OK"
+    history = datetime.now() - timedelta(days=2)
+    docs = client.collection("updates").select(["stat", "value"]).where("timestamp", ">", history).get()
+    return json.dumps([doc.to_dict() for doc in docs])
