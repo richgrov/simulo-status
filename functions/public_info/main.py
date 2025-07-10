18b6ff0ffb3bd367798ba12f59c40ccc2277e529
Richard Grover <38992201+richgrov@users.noreply.github.com>
2025-07-10 11:19:08 -0600

    Code cleanup public_info function

--- functions/public_info/main.py
+++ functions/public_info/main.py
@@ -1,13 +1,14 @@
 from datetime import datetime, timedelta
 import json
 
+import flask
 import functions_framework
 from google.cloud import firestore
 
 client = firestore.Client(database="status-db")
 
 @functions_framework.http
-def main(request):
+def main(_: flask.Request):
     history = datetime.now() - timedelta(days=2)
     docs = client.collection("updates").select(["stat", "value"]).where("timestamp", ">", history).get()
     return json.dumps([doc.to_dict() for doc in docs])
