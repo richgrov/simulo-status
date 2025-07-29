c2bc398a50f14889776ab8264b1dfeead5c3a4d6
Richard Grover <38992201+richgrov@users.noreply.github.com>
2025-07-29 12:08:57 -0600

    Change icon based on status

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,10 @@ import "./admin";
 
 const statusText = document.getElementById("status-text")!;
 const statusTime = document.getElementById("status-time")!;
+const circle = document.getElementById("circle")!;
+const checkIcon = document.getElementById("check-icon")!;
+const warningIcon = document.getElementById("warning-icon")!;
+const xIcon = document.getElementById("x-icon")!;
 
 try {
   const response = await fetch(process.env.VITE_BACKEND_URL);
@@ -10,8 +14,12 @@ try {
 
   if (data.status === "ok") {
     statusText.textContent = "ONLINE";
+    circle.style.backgroundColor = "#2ecc71";
+    checkIcon.style.display = "block";
   } else {
     statusText.textContent = "SERVICE DEGRADATION";
+    circle.style.backgroundColor = "#FF6900";
+    warningIcon.style.display = "block";
   }
 
   if (data.since) {
@@ -20,4 +28,6 @@ try {
 } catch (error) {
   console.error(error);
   statusText.textContent = "SERVICE ERROR";
+  circle.style.backgroundColor = "#fa2c37";
+  xIcon.style.display = "block";
 }
